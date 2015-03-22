d23fc070b251cc0c0dc72a950775cb85ca91140c
Your Name <you@example.com>
2015-03-22 23:42:23 +0000

    bug fix: Now accounts for spaces between elements.

--- lib/rexle-diff.rb
+++ lib/rexle-diff.rb
@@ -3,7 +3,7 @@
 # file: rexle-diff.rb
 
 require 'rexle'
-require 'rexle-builder'
+
 
 class RexleDiff
 
@@ -11,8 +11,8 @@ class RexleDiff
 
   def initialize(source1, source2, ignore_removed: true)    
 
-    a = timestamp(source1).to_a
-    a2 = source2.to_a
+    a = timestamp(Rexle.new(source1)).to_a
+    a2 = RexleParser.new(source2).to_a
 
     compare(a, a2)
 
@@ -22,40 +22,67 @@ class RexleDiff
   private
 
   def names_values(c)
-    c.inject([[],[]]){|r, x| r.first << x[0]; r.last << x[2]; r}
+    
+    c.inject([[],[]]) {|r, x|  r.first << x[0];  r.last << x[2]; r }
+
   end
 
   def added(list, list2)
 
     result = list2 -  list
-    indexes = result.map{|x| list2.index x}
+    indexes = result.map {|x| list2.index x}
 
+  end
+  
+  def array_index(a,i)
+    a2 = a.select{|x| x.is_a? Array}
+    a.index a2[i]
   end
 
   def compare(a, a2)
-
-    _, _, _, *c = a
+          _, _, _, *raw_c = a
+    c = raw_c.select{|x| x.is_a? Array}
     names, values = names_values(c)
-    _, _, _, *c2 = a2
+        
+    _, _, _, *raw_c2 = a2
+    c2 = raw_c2.select{|x| x.is_a? Array}
     names2, values2 = names_values(c2)
 
     added_indexes = added(values, values2)
 
-    added_indexes.each do |i| 
-      a2[3+i][1].merge!(created: Time.now, last_modified: Time.now)
-    end
+    added_indexes.each do |i|
 
-    unchanged_indexes = (0..c.length-1).to_a - added_indexes
+      offset = array_index(a2, i)
+      a2[offset][1].merge!(created: Time.now, last_modified: Time.now)
+    end
+    
+    # we need to know the deleted index
+    deleted_indexes = deleted(values, values2)
+    
+    unchanged_indexes = unchanged(values, values2)
+    #puts 'c : ' + c.inspect
+    #puts 'added: ' + added_indexes.inspect
+    #puts 'deleted :'  + deleted_indexes.inspect
+    #puts 'unchanged : ' + unchanged_indexes.inspect
 
     # check the child element if any
-
-    unchanged_indexes.each do |i|      
-
-      a2[3+i][1].merge!(a[3+i][1])
+    
+    unchanged_indexes.each do |i, i2|      
+          
+      offset = array_index(a, i)
+      offset2 = array_index(a2, i2)
+      a2[offset2][1].merge!(a[offset][1])
       compare(c[i],c2[i]) if c[i].length > 3
     end
 
   end
+  
+  def deleted(list, list2)
+
+    result = list -  list2
+    indexes = result.map {|x| list.index x}
+
+  end  
 
   def timestamp(doc)
 
@@ -65,6 +92,15 @@ class RexleDiff
 
     return doc
   end
+  
+  def unchanged(list, list2)
+
+    result = list &  list2
+    indexes = result.map {|x| list.index x}
+    indexes2 = result.map {|x| list2.index x}
+    
+    indexes.zip(indexes2)
+
+  end    
 
 end
-
--- rexle-diff.gemspec
+++ rexle-diff.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rexle-diff'
-  s.version = '0.1.0'
+  s.version = '0.1.1'
   s.summary = 'rexle-diff'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
